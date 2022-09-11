93388a49f1c614309ccb77e0ea8f45943dec4d81
pp <ichikawa929@yahoo.co.jp>
2022-09-12 00:51:54 +0900

    fix: update main.go

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+	"github.com/pp-develop/make-playlist-by-specify-time-api/pkg"
 )
 
-func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, req.FormValue("minute"))
-	}
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
 
-	http.HandleFunc("/make-playlist", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+    router.GET("/playlist", makePlaylist)
+
+    router.Run(":8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
+}
+
+func makePlaylist(c *gin.Context) {
+    // Get a greeting message and print it.
+    message := make_playlist.Hello("Gladys")
+    c.IndentedJSON(http.StatusOK, message)
 }
---
+++ pkg/go.mod
@@ -0,0 +1,3 @@
+module github.com/pp-develop/make-playlist-by-specify-time-api/pkg
+
+go 1.19
