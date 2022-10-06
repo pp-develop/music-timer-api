6f1b5cffc8d09b3cbd85eac9b7760623f32f1ae9
pp <ichikawa929@yahoo.co.jp>
2022-10-07 00:08:10 +0900

    fix: update create_playlist.go

--- internal/create_playlist.go
+++ internal/create_playlist.go
@@ -3,10 +3,9 @@ package internal
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"log"
+	"io"
 	"net/http"
-	"net/http/httputil"
+	"strconv"
 )
 
 type RequestBody struct {
@@ -14,23 +13,36 @@ type RequestBody struct {
 	Description string `json:"description"`
 }
 
-func CreatePlaylist(userid string, token string) {
+type Response struct {
+	Id string `json:"id"`
+}
+
+func CreatePlaylist(userid string, ms int, token string) (bool, string) {
 	requestBody := &RequestBody{
-		Name:        "pp!!!",
-		Description: "test",
+		Name:        strconv.Itoa(ms/60000) + "分",
+		Description: "",
 	}
 	jsonString, _ := json.Marshal(requestBody)
-	
+
 	endopint := "https://api.spotify.com/v1/users/" + userid + "/playlists"
 	req, _ := http.NewRequest("POST", endopint, bytes.NewBuffer(jsonString))
 	req.Header.Set("Authorization", "Bearer "+token)
 	client := new(http.Client)
 	resp, err := client.Do(req)
-	dumpResp, _ := httputil.DumpResponse(resp, true)
-	fmt.Printf("%s", dumpResp)
-
 	if err != nil {
-		log.Println("httprequest error", err)
+		return false, ""
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		return false, ""
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return false, ""
+	}
+	return true, response.Id
 }
