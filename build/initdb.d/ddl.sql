20fae9898af459228f5bf0dfbbd876b80f520624
pp <ichikawa929@yahoo.co.jp>
2022-11-21 01:41:33 +0900

    fix: update ddl

--- build/initdb.d/ddl.sql
+++ build/initdb.d/ddl.sql
@@ -1,16 +1,17 @@
 create table tracks(
-    `uri` VARCHAR(255), index uri_index (uri),
+    `uri` VARCHAR(255),
+    index uri_index (uri),
     `artists` text,
     `popularity` int,
     `duration_ms` int,
     `isrc` VARCHAR(255),
     `created_at` datetime
-    );
+);
 
 create table playlist(
     `id` int not null auto_increment primary key,
-    `msec` int,
-)
+    `msec` int
+);
 
 create table users(
     `id` VARCHAR(255) primary key,
@@ -20,4 +21,4 @@ create table users(
     `session` VARCHAR(255),
     `created_at` datetime,
     `updated_at` datetime
-    );
+);
