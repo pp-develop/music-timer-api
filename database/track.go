bd33beeeb99c6740b7ef25a09dd3131919e7100e
pp <ichikawa929@yahoo.co.jp>
2023-02-07 05:24:12 +0900

    fix: update save track

--- database/track.go
+++ database/track.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"strings"
+
 	"github.com/pp-develop/make-playlist-by-specify-time-api/model"
 	"github.com/zmb3/spotify/v2"
 )
@@ -30,7 +32,7 @@ func GetTrackByMsec(ms int) []model.Track {
 	var tracks []model.Track
 	var track model.Track
 
-	if err := db.QueryRow("SELECT uri, duration_ms FROM tracks WHERE duration_ms BETWEEN ?-30000 AND ?+30000 AND isrc LIKE 'JP%' ORDER BY rand()", ms, ms).Scan(&track.Uri, &track.DurationMs); err != nil {
+	if err := db.QueryRow("SELECT uri, duration_ms FROM tracks WHERE duration_ms = ? AND isrc LIKE 'JP%' ORDER BY rand()", ms).Scan(&track.Uri, &track.DurationMs); err != nil {
 		return tracks
 	}
 
@@ -39,7 +41,12 @@ func GetTrackByMsec(ms int) []model.Track {
 }
 
 func SaveTrack(track *spotify.FullTrack) error {
-	_, err := db.Exec("INSERT INTO tracks (uri, duration_ms, isrc, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW()) ON DUPLICATE KEY UPDATE updated_at = NOW()", track.URI, track.Duration, track.ExternalIDs["isrc"])
+	var artistName []string
+	for _, v := range track.Album.Artists {
+		artistName = append(artistName, v.Name)
+	}
+
+	_, err := db.Exec("INSERT INTO tracks (uri, artists_name, duration_ms, isrc, created_at, updated_at) VALUES (?, ?, ?, ?, NOW(), NOW()) ON DUPLICATE KEY UPDATE updated_at = NOW()", track.URI, strings.Join(artistName, " "), track.Duration, track.ExternalIDs["isrc"])
 	if err != nil {
 		return err
 	}
