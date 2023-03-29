a1132fe04e159cd619d4c2b56c8612e3f3030b46
pp <ichikawa929@yahoo.co.jp>
2023-03-30 02:37:43 +0900

    Deserialize JSON data to 'track' type

--- pkg/json/read.go
+++ pkg/json/read.go
@@ -1,37 +1,63 @@
 package json
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
 )
 
-// JSONファイルの中身をキーで指定して配列型の中身を取得する
-func Read(key string) ([]interface{}, error) {
-    // ファイルの読み込み
-    bytes, err := ioutil.ReadFile(filePath)
-    if err != nil {
-        return nil, err
-    }
+type Track struct {
+	Id          string `json:"id"`
+	Description int    `json:"description"`
+}
 
-    // JSONのパース
-    var data map[string]interface{}
-    err = json.Unmarshal(bytes, &data)
-    if err != nil {
-        return nil, err
-    }
+func Read(key string) ([]Track, error) {
+	// ファイルの読み込み
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
 
-    // 配列型のキーを取得
-    value, ok := data[key]
-    if !ok {
-        return nil, fmt.Errorf("Key %s not found", key)
-    }
+	// JSONのパース
+	var data map[string]interface{}
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
 
-    // 配列にキャスト
-    array, ok := value.([]interface{})
-    if !ok {
-        return nil, fmt.Errorf("Key %s is not an array", key)
-    }
+	// 配列型のキーを取得
+	value, ok := data[key]
+	if !ok {
+		return nil, fmt.Errorf("Key %s not found", key)
+	}
 
-    return array, nil
+	// 配列にキャスト
+	array, ok := value.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("Key %s is not an array", key)
+	}
+
+	// []interface{}型の配列を[]Track型に変換
+	tracks := make([]Track, 0, len(array))
+	for _, v := range array {
+		item := v.(map[string]interface{})
+		track := Track{
+			Id:          item["id"].(string),
+			Description: int(item["description"].(float64)), // float64型をint型に変換
+		}
+		tracks = append(tracks, track)
+	}
+
+	return tracks, nil
+}
+
+func ShuffleTracks(tracks []Track) []Track {
+	// Fisher-Yates アルゴリズムを使って、スライスの要素をランダムに並び替える
+	n := len(tracks)
+	for i := n - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		tracks[i], tracks[j] = tracks[j], tracks[i]
+	}
+	return tracks
 }
