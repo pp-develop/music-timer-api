23e6a61d7b1f5e13bd79a986872065daaefb260b
pp <ichikawa929@yahoo.co.jp>
2023-05-02 11:21:39 +0900

    Add pagination to retrieve track data

--- database/track.go
+++ database/track.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/pp-develop/make-playlist-by-specify-time-api/model"
@@ -20,25 +21,58 @@ func SaveTrack(track *spotify.FullTrack) error {
 	return nil
 }
 
-func GetAllTracks() ([]model.Track, error) {
-	var tracks []model.Track
-	rows, err := db.Query("SELECT uri, duration_ms, artists_name FROM tracks WHERE isrc like 'JP%' ORDER BY rand()")
+// ページ番号とページサイズに基づいてトラックを取得する関数
+func GetTracks(pageNumber, pageSize int) ([]model.Track, error) {
+	// OFFSETを計算してLIMIT句を生成
+	offset := (pageNumber - 1) * pageSize
+	limit := pageSize
+
+	// クエリ実行
+	query := "SELECT uri, duration_ms, artists_name FROM tracks WHERE isrc like 'JP%' LIMIT " + strconv.Itoa(offset) + "," + strconv.Itoa(limit)
+	rows, err := db.Query(query)
 	if err != nil {
-		return tracks, err
+		return nil, err
 	}
 	defer rows.Close()
 
+	tracks := make([]model.Track, 0)
 	for rows.Next() {
 		var track model.Track
 		if err := rows.Scan(&track.Uri, &track.DurationMs, &track.ArtistsName); err != nil {
-			return tracks, err
+			return nil, err
 		}
 		tracks = append(tracks, track)
 	}
-	if err = rows.Err(); err != nil {
-		return tracks, err
+	if err := rows.Err(); err != nil {
+		return nil, err
 	}
 	return tracks, nil
+}
+
+func GetAllTracks() ([]model.Track, error) {
+	var AllTracks []model.Track
+
+	// ページネーションでトラックデータを取得
+	pageSize := 50000
+	pageNumber := 1
+	for {
+		// ページ番号とページサイズに基づいてトラックを取得
+		tracks, err := GetTracks(pageNumber, pageSize)
+		if err != nil {
+			return nil, err
+		}
+
+		// 取得したトラックデータを処理
+		AllTracks = append(AllTracks, tracks...)
+
+		// ページネーション処理のために、ページ番号をインクリメントして次のページのトラックデータを取得
+		pageNumber++
+		if len(tracks) < pageSize {
+			// ページネーション処理が終了した場合はループを終える
+			break
+		}
+	}
+	return AllTracks, nil
 }
 
 func GetTracksByArtistsName(artistsName string) ([]model.Track, error) {
