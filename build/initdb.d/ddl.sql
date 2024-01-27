49af30f231e3f2fe8cae1f2e4b17855e322621e7
pp <ichikawa929@yahoo.co.jp>
2024-01-28 05:22:25 +0900

    Update table definitions

--- build/initdb.d/ddl.sql
+++ build/initdb.d/ddl.sql
@@ -1,5 +1,16 @@
+create table users(
+    `id` VARCHAR(255) primary key,
+    `access_token` text,
+    `refresh_token` text,
+    `token_expiration` int,
+    `session` VARCHAR(255),
+    `created_at` datetime,
+    `updated_at` datetime
+);
+
 create table tracks(
     `uri` VARCHAR(255) primary key,
+    `artists_id` json,
     `artists_name` json,
     `duration_ms` int,
     `isrc` VARCHAR(255),
@@ -8,6 +19,7 @@ create table tracks(
 );
 
 create table artists(
+    `id` VARCHAR(255) primary key,
     `user_id` VARCHAR(255),
     `name` text,
     FOREIGN KEY fk_user_id(user_id) REFERENCES users(id),
@@ -19,14 +31,4 @@ create table playlists(
     index id_index (id),
     `user_id` VARCHAR(255),
     FOREIGN KEY fk_user_id(user_id) REFERENCES users(id)
-);
-
-create table users(
-    `id` VARCHAR(255) primary key,
-    `access_token` text,
-    `refresh_token` text,
-    `token_expiration` int,
-    `session` VARCHAR(255),
-    `created_at` datetime,
-    `updated_at` datetime
 );
