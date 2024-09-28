558a88be438a31170b9c1468b80d21bb96afa5ef
pp <ichikawa929@yahoo.co.jp>
2024-09-28 12:46:30 +0900

    Fix initialization process for track data

--- pkg/json/recreate.go
+++ pkg/json/recreate.go
@@ -3,19 +3,12 @@ package json
 import (
 	"log"
 
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/pp-develop/music-timer-api/model"
 	"github.com/pp-develop/music-timer-api/utils"
 )
 
 func ReCreate(c *gin.Context) error {
-	session := sessions.Default(c)
-	v := session.Get("userId")
-	if v == nil {
-		return model.ErrFailedGetSession
-	}
-
 	db, ok := utils.GetDB(c)
 	if !ok {
 		return model.ErrFailedGetDB
