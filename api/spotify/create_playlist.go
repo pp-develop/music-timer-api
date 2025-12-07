1a23f2b59051957a58fbdd61a67afbde915e5d5b
pp <ichikawa929@yahoo.co.jp>
2025-12-07 10:00:14 +0900

    Refactor CreatePlaylist function to utilize common track constants for duration calculation
    - Replaced hardcoded milliseconds per minute with a constant from the common track package for improved maintainability and clarity in duration handling.

--- api/spotify/create_playlist.go
+++ api/spotify/create_playlist.go
@@ -5,17 +5,15 @@ import (
 	"strconv"
 
 	"github.com/pp-develop/music-timer-api/model"
+	commontrack "github.com/pp-develop/music-timer-api/pkg/common/track"
 	"github.com/zmb3/spotify/v2"
 )
-
-// MillisecondsPerMinute は1分あたりのミリ秒数
-const MillisecondsPerMinute = 60000
 
 // CreatePlaylist creates a new playlist for the user with the specified duration
 func CreatePlaylist(ctx context.Context, user model.User, ms int) (*spotify.FullPlaylist, error) {
 	client := NewClientWithUser(ctx, user)
 
-	playlist, err := client.CreatePlaylistForUser(ctx, user.Id, strconv.Itoa(ms/MillisecondsPerMinute)+"min", "", true, false)
+	playlist, err := client.CreatePlaylistForUser(ctx, user.Id, strconv.Itoa(ms/commontrack.MillisecondsPerMinute)+"min", "", true, false)
 	if err != nil {
 		return playlist, WrapSpotifyError(err, model.ErrPlaylistCreationFailed)
 	}
